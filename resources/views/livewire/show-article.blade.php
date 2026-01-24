d198ea19183904099165912431cce32c97844cfe
GitJDGT <GitJDGT@users.noreply.github.com>
2026-01-24 14:08:33 -0500

    ### fix(ui): remove redundant comment counter and adjust profile image size
    - Removed the comment counter icon from the top section to avoid redundancy.
    - Slightly increased the user profile image size in `show-article.blade.php`.

--- resources/views/livewire/show-article.blade.php
+++ resources/views/livewire/show-article.blade.php
@@ -33,20 +33,13 @@
                         </span>
                         <span class="flex items-center">
                             @if($article->author->image)
-                                <img src="{{ Storage::url($article->author->image) }}" alt="{{ $article->author->name }}" class="w-6 h-6 rounded-full mr-2 object-cover">
+                                <img src="{{ Storage::url($article->author->image) }}" alt="{{ $article->author->name }}" class="w-8 h-8 md:w-10 md:h-10 rounded-full mr-2 object-cover">
                             @else
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
                                   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                 </svg>
                             @endif
                             {{ $article->author->name }}
-                        </span>
-                        {{-- Contador de comentarios si quieres --}}
-                        <span class="flex items-center">
-                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                               <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                             </svg>
-                            {{ $article->comments->count() }}
                         </span>
                     </div>
                     {{-- Botones de compartir: parte superior --}}
